73f18101687efd7f11f334ce16846a07b88557c0
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-05 16:30:07 +0100

    feat: add style to iframe tag in Media.js

--- nasa-app/src/components/Media.js
+++ nasa-app/src/components/Media.js
@@ -1,15 +1,20 @@
-import PropTypes from 'prop-types'
+import React from 'react';
 
 const Media = ({planet}) => {
     const type = planet.media_type;
     const urladdress = planet.url;
+    const emb = urladdress.replace("http://www.youtube.com/watch?v=", "http://www.youtube.com/embed/");
 
     if (type === 'image') {
         return (<img src={urladdress} alt="planet"/>)
     }
-    if(type != 'image') {
+
+    if(type !== 'image') {
         return (
-            <iframe src={urladdress}></iframe>
+            <iframe width="960" height="540" src={emb}
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen=""></iframe>
         )
     }
 }
