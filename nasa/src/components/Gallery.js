cfed8f37d187cc2da84fec1eef26e926aefe0b98
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-07 17:01:52 +0100

    feat: implemented Gallery.js

--- nasa/src/components/Gallery.js
+++ nasa/src/components/Gallery.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
-
+import MyVerticallyCenteredModal from './MyVerticallyCenteredModal'
 
 const Gallery = ({gallery}) => {
+    const [displayModal, setDisplayModal] = useState(false)
+    const [currentPlanet, setCurrentPlanet] = useState([])
+
+    const handleClickGallery = (event, planet) => {
+        setDisplayModal(!displayModal)
+        setCurrentPlanet(planet)
+        let a = event.target;
+        console.log('cliked');
+        console.log(a);
+        console.log(planet)
+    }
+
+    //iframe too!
    return (
         <>
             <h2> Gallery</h2>
             <div className='gallery'>
             {gallery.map((planet, index) => (
-                <img key={index} src={planet.url} alt="planet"/>
+                <img  key={index} src={planet.url} alt="planet" onClick={
+                    (event) => handleClickGallery(event, planet)}/>
             ))}
+            </div>
+            <div>
+                {!displayModal ? (
+                    <p></p>
+                ):(
+                   <MyVerticallyCenteredModal planet={currentPlanet}  >
+
+                   </MyVerticallyCenteredModal>
+                )}
+
             </div>
         </>
     )
